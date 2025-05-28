fbd43070898de0add68e6a3603351f37e692d179
40347024s <40347024s@ntnu.edu.tw>
2025-05-28 15:26:04 +0800

    fix: module gym_2048_ver2023 not found issue.

--- gym_2048/__init__.py
+++ gym_2048/__init__.py
@@ -2,18 +2,18 @@ from gymnasium.envs.registration import register
 
 register(
     id='Game2048-v0',
-    entry_point='gym_2048_ver2023.envs:Game2048Env',
+    entry_point='gym_2048.envs:Game2048Env',
     max_episode_steps=10000,
 )
 
 register(
     id='Norm2048-v0',
-    entry_point='gym_2048_ver2023.envs:NormGame2048Env_ver0',
+    entry_point='gym_2048.envs:NormGame2048Env_ver0',
     max_episode_steps=10000,
 )
 
 register(
     id='Norm2048-v1',
-    entry_point='gym_2048_ver2023.envs:NormGame2048Env_ver1',
+    entry_point='gym_2048.envs:NormGame2048Env_ver1',
     max_episode_steps=10000,
 )
